2ea38f87c66885b20655dd7922e80a3062f3e1a3
Sebastian Scherbes <github@scherbes.de>
2023-10-20 11:49:43 +0200

    create trashbox button

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings } from "lucide-react";
-import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState, useEffect } from "react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { useMediaQuery } from "usehooks-ts";
-import { UserItem } from "./user-item";
 import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Item } from "./item";
+import { usePathname } from "next/navigation";
 import { toast } from "sonner";
+
+import { cn } from "@/lib/utils";
+import { api } from "@/convex/_generated/api";
+
+import { UserItem } from "./user-item";
+import { Item } from "./item";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -129,6 +132,14 @@ const Navigation = () => {
         <div className="mt-4">
           <DocumentList />
           <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent side={isMobile ? "bottom" : "right"} className="p-0 w-72">
+              <p>Trash box</p>
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
